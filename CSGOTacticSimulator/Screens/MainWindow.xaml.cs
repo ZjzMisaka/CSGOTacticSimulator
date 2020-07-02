ec329d8b1cc2f1553e40c30164b32865dd455672
ZjzMisaka <530514257@qq.com>
2020-07-02 19:26:03 +0800

    fix bug
    1. 修复了炸弹爆炸时的一些bug

--- CSGOTacticSimulator/Screens/MainWindow.xaml.cs
+++ CSGOTacticSimulator/Screens/MainWindow.xaml.cs
@@ -2543,8 +2543,8 @@ namespace CSGOTacticSimulator
                             explosionImage.Source = new BitmapImage(new Uri(GlobalDictionary.explosionPath));
                             explosionImage.Width = GlobalDictionary.ExplosionEffectWidthAndHeight;
                             explosionImage.Height = GlobalDictionary.ExplosionEffectWidthAndHeight;
-                            Point explosionWndPoint = new Point(Canvas.GetTop(bombImage), Canvas.GetLeft(bombImage));
-                            c_runcanvas.Children.Remove(explosionImage);
+                            Point explosionWndPoint = new Point(Canvas.GetLeft(bombImage), Canvas.GetTop(bombImage));
+                            c_runcanvas.Children.Remove(bombImage);
 
                             Canvas.SetLeft(explosionImage, explosionWndPoint.X);
                             Canvas.SetTop(explosionImage, explosionWndPoint.Y);
